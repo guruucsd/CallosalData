afe48d562fd595fae5425c9bad0315f3ffc46443
Ben Cipollini <bcipolli@ucsd.edu>
2015-03-26 13:46:17 -0700

    Add fig1b, parts 3 and 4.

--- code/aboitiz_etal_1992/ab_densities.m
+++ code/aboitiz_etal_1992/ab_densities.m
@@ -118,6 +118,116 @@ function vars = ab_data(validate_data)
 
 
 
+    %% Fig 1b #3
+    [img,orig_img] = scrub_image(fullfile(AB_img_dirpath, 'Fig1b-3.png'), -0);
+
+    % Find x axes
+    [gx,gxs] = get_groups(sum(img,2) > size(img,2)*0.75, 'right'); xaxis_last = gx(2); xaxis_first = gx(2)-gxs(2);
+    [gy,gys] = get_groups(sum(img,1) > size(img,1)*0.75, 'left');  yaxis_first = gy(2); yaxis_last = gy(2)+gys(2);
+
+    data_pix = img(gx(1)+2:xaxis_first-2, gy(1)+gys(1)+1:yaxis_first-2);
+    j = get_groups(sum(data_pix,2),'right');
+    data_pix = data_pix(j(1)+1:end,:);
+
+    % Get x ticks after that
+    % find x and y ticks
+    yticks = get_groups(data_pix(:,end-2));
+    xticks = get_groups(data_pix(end, :));
+
+    if visualize_data
+        figure; imshow(data_pix); hold on;
+        plot(xticks,(size(data_pix,1)-1)*ones(size(xticks)),'*g');
+        plot(size(data_pix,2)-1, yticks, '*g');
+    end;
+ 
+    dens_pix = zeros(size(xticks));
+    for xi = 1:length(xticks)
+        xsliver = data_pix(:, max(1,floor(xticks(xi)-3)):min(size(data_pix,2),floor(xticks(xi)+3)));
+        xsum    = sum(xsliver,2);
+        [gg,ggs] = get_groups(xsum==max(xsum));
+        if visualize_data
+            plot(xticks(xi), gg, 'y*');
+        end;
+        if length(gg) > 3, gg = gg(end-2:end); end; % picked up the text
+        if length(gg)~=3, xi, gg, error('?'); end;
+        dens_pix(xi) = gg(2); % we subtracted off 4 pixels above
+    end;
+    if visualize_data
+        plot(xticks, dens_pix, 'r*')
+    end;
+    
+    % Get final values
+    ab_fig1_g3_dens_vals = (5 + 5*(yticks(end)-dens_pix)/mean(diff(yticks)))*1E2;
+
+    % Plot old vs. new values
+    if validate_data
+        figure; set(gcf,'Position',[104         516        1093         158]);
+        subplot(1,2,1);
+        imshow(orig_img);
+        subplot(1,2,2);
+        plot(1:length(ab_fig1_cc_regions), ab_fig1_g3_dens_vals, '-o');
+        set(gca, 'xtick',1:length(ab_fig1_cc_regions), 'xticklabel', ab_fig1_cc_regions, 'xlim', [0.5
+        length(ab_fig1_cc_regions)+0.5], 'ylim', 1E2*[0 10.]);
+    end;
+
+
+
+    %% Fig 1b #4
+    [img,orig_img] = scrub_image(fullfile(AB_img_dirpath, 'Fig1b-4.png'), -0);
+
+    % Find x axes
+    [gx,gxs] = get_groups(sum(img,2) > size(img,2)*0.75, 'right'); xaxis_last = gx(2); xaxis_first = gx(2)-gxs(2);
+    [gy,gys] = get_groups(sum(img,1) > size(img,1)*0.75, 'left');  yaxis_first = gy(2); yaxis_last = gy(2)+gys(2);
+
+    data_pix = img(gx(1)+2:xaxis_first-2, gy(1)+gys(1)+1:yaxis_first-2);
+    j = get_groups(sum(data_pix,2),'right');
+    data_pix = data_pix(j(1)+1:end,:);
+
+    % Get x ticks after that
+    % find x and y ticks
+    yticks = get_groups(data_pix(:,end-2));
+    xticks = get_groups(data_pix(end, :));
+
+    if visualize_data
+        figure; imshow(data_pix); hold on;
+        plot(xticks,(size(data_pix,1)-1)*ones(size(xticks)),'*g');
+        plot(size(data_pix,2)-1, yticks, '*g');
+    end;
+
+    dens_pix = zeros(size(xticks));
+    for xi = 1:length(xticks)
+        xsliver = data_pix(:, max(1,floor(xticks(xi)-3)):min(size(data_pix,2),floor(xticks(xi)+3)));
+        xsum    = sum(xsliver,2);
+        [gg,ggs] = get_groups(xsum==max(xsum));
+        if visualize_data
+            plot(xticks(xi), gg, 'y*');
+        end;
+        if length(gg) > 3, gg = gg(end-2:end); % picked up the text
+        elseif length(gg) == 1, gg=[0 gg]; end;
+        if visualize_data
+            ens_pix(xi) = gg(2); % we subtracted off 4 pixels above
+        end;
+        dens_pix(xi) = gg(2); % we subtracted off 4 pixels above
+    end;
+    if visualize_data
+        plot(xticks, dens_pix, 'r*')
+    end;
+    
+    % Get final values
+    ab_fig1_g5_dens_vals = (1 + 1*(yticks(end)-dens_pix)/mean(diff(yticks)))*1E2;
+
+    % Plot old vs. new values
+    if validate_data
+        figure; set(gcf,'Position',[104         516        1093         158]);
+        subplot(1,2,1);
+        imshow(orig_img);
+        subplot(1,2,2);
+        plot(1:length(ab_fig1_cc_regions), ab_fig1_g5_dens_vals, '-o');
+        set(gca, 'xtick',1:length(ab_fig1_cc_regions), 'xticklabel', ab_fig1_cc_regions, 'xlim', [0.5
+        length(ab_fig1_cc_regions)+0.5], 'ylim', 1E2*[0 3.5]);
+    end;
+
+
     % Determined this through manual image editing interventions
     ab_fig1_cc_areas = [27554 12007 12106 7952 7806 9128 8969 10858 15946 13889];
     ab_fig1_cc_rel_areas = ab_fig1_cc_areas./sum(ab_fig1_cc_areas);
